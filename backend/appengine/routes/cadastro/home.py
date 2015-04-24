9fb56cc4c2ca27a8d3a1afff6a6e333287b66d85
Paulo <henrique.paulo307@hotmail.com>
2015-04-24 11:10:50 -0300

    salvando em banco

--- backend/appengine/routes/cadastro/home.py
+++ backend/appengine/routes/cadastro/home.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
 from gaecookie.decorator import no_csrf
+from gaeforms import base
+from gaeforms.base import Form
+from gaegraph.model import Node
 from gaepermission.decorator import login_not_required
 from config.template_middleware import TemplateResponse
 from tekton import router
@@ -19,7 +23,33 @@ def form():
     return TemplateResponse(contexto)
 
 
+class Login(Node):
+    name = ndb.StringProperty(required=True)#required = true significa que é obrigatório
+    email = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
+
+class LoginForm(Form):
+    name = base.StringField(required=True)
+    email = base.StringField(required=True)
+    password = base.StringField(required=True)
+
 @login_not_required
 @no_csrf
 def salvar(_resp, **propriedades):
-    _resp.write(propriedades)
+    login_form = LoginForm(**propriedades)
+    erros = login_form.validate()
+    if erros:
+        _resp.write(erros)
+    else:
+        pass
+        login=Login(name=propriedades['name'],
+                    email=propriedades['email'],
+                    password=propriedades['password'])
+        login.put()
+        _resp.write(propriedades)
+
+#feito para mostrar o que esta sendo salvo no banco
+#@login_not_required
+#@no_csrf
+#def salvar(_resp, **propriedades):
+#    _resp.write(propriedades)
